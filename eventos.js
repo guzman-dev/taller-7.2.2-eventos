c329f9dbe04ae21804866466ce50561d382de895
Guzmán <gprietoroman3@gmail.com>
2025-08-08 20:45:19 -0300

    Solucioné el event bubbling

--- eventos.js
+++ eventos.js
@@ -1,6 +1,8 @@
 let divDeBoton = document.getElementById("divDeBoton");
+let boton = document.getElementById("boton");
 divDeBoton.addEventListener("click", saludaElDiv);
 
 function saludaElDiv(){
     alert("Hola! Soy el div");
+    
 }
